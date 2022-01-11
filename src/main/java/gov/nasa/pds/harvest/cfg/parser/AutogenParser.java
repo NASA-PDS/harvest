28b0f5a954b128cca791cfe57e47065f05823405
Eugene <test@test.com>
2022-01-11 09:41:20 -0800

    Manifest with a list of files

--- src/main/java/gov/nasa/pds/harvest/cfg/parser/AutogenParser.java
+++ src/main/java/gov/nasa/pds/harvest/cfg/parser/AutogenParser.java
@@ -1,11 +1,16 @@
 package gov.nasa.pds.harvest.cfg.parser;
 
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 import gov.nasa.pds.harvest.cfg.model.AutogenCfg;
 import gov.nasa.pds.harvest.util.xml.XPathUtils;
+import gov.nasa.pds.harvest.util.xml.XmlDomUtils;
 
 
 /**
@@ -15,6 +20,16 @@ import gov.nasa.pds.harvest.util.xml.XPathUtils;
  */
 public class AutogenParser
 {
+    /**
+     * Valid &lt;autogenFields&gt; attribute names.
+     */
+    private static Set<String> AUTOGEN_ATTRS = new TreeSet<>();
+    static
+    {
+        AUTOGEN_ATTRS.add("generate");
+    }
+
+    
     public static AutogenCfg parseAutogenFields(Document doc) throws Exception
     {
         XPathUtils xpu = new XPathUtils();
@@ -23,6 +38,26 @@ public class AutogenParser
         int count = xpu.getNodeCount(doc, "/harvest/autogenFields");
         if(count == 0) return cfg;
         if(count > 1) throw new Exception("Could not have more than one '/harvest/autogenFields' element.");
+        
+        // Attributes
+        Node node = xpu.getFirstNode(doc, "/harvest/autogenFields");
+        validateAttributes(node, AUTOGEN_ATTRS);
+
+        String str = XmlDomUtils.getAttribute(node, "generate");
+        if(str == null)
+        {
+            cfg.generate = true;
+        }
+        else
+        {
+            Boolean bb = ConfigParserUtils.parseBoolean(str);
+            if(bb == null)
+            {
+                throw new Exception("Invalid <autogenFields generate=\"" + str + "\" attribute.");
+            }
+            
+            cfg.generate = bb;
+        }
 
         // Class filter
         cfg.classFilterIncludes = xpu.getStringSet(doc, "/harvest/autogenFields/classFilter/include");
@@ -44,4 +79,19 @@ public class AutogenParser
 
         return cfg;
     }
+
+
+    private static void validateAttributes(Node node, Set<String> values) throws Exception
+    {
+        NamedNodeMap atts = XmlDomUtils.getAttributes(node);
+        for(int i = 0; i < atts.getLength(); i++)
+        {
+            String attName = atts.item(i).getNodeName();
+            if(!values.contains(attName))
+            {
+                throw new Exception("<" + node.getNodeName() + "> element has invalid attribute '" + attName + "'");
+            }
+        }
+    }
+
 }
