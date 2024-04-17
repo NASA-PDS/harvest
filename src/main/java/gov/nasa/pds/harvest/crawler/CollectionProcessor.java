ca3e0c6d96a66d355e27926c74016b84c34e27a5
Al Niessner <Al.Niessner@xxx.xxx>
2024-04-17 12:14:27 -0700

    fix the fixme

--- src/main/java/gov/nasa/pds/harvest/crawler/CollectionProcessor.java
+++ src/main/java/gov/nasa/pds/harvest/crawler/CollectionProcessor.java
@@ -131,7 +131,6 @@ public class CollectionProcessor extends BaseProcessor
         meta.setNodeName(config.getNodeName().toString());
 
         // Collection filter
-        // FIXME: this needs to be changed and may relate to other fixme in this file
         List<String> lids = ConfigManager.exchangeLids (bCfg.getCollection());
         List<String> lidvids = ConfigManager.exchangeLidvids (bCfg.getCollection());
         if(!lids.isEmpty() && !lids.contains(meta.lid)) return;
